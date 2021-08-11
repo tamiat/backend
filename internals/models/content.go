6d1eadd781d226fd4bda6aebd0f223574dc45d59
RahmaYasser <rahma.y9079@gmail.com>
2021-08-11 18:09:57 +0200

    deleteing static data

--- internals/models/content.go
+++ internals/models/content.go
@@ -5,9 +5,3 @@ type Content struct {
 	Title   string `json:"title"`
 	Details string `json:"details"`
 }
-
-var ContentArr = []Content{
-	{Id: 0, Title: "hello world", Details: "Welcome to Tamiat CMS!"},
-	{Id: 1, Title: "hello world 1", Details: "Welcome to Tamiat CMS!"},
-	{Id: 2, Title: "hello world 2", Details: "Welcome to Tamiat CMS!"},
-}
