b88b66cc137cf57177c28bfb09a9e60227e20a71
RahmaYasser <rahma.y9079@gmail.com>
2021-08-17 22:21:43 +0200

    adding content model

---
+++ domain/content/content.go
@@ -0,0 +1,12 @@
+package content
+
+type Content struct {
+	Id      int    `json:"id"`
+	Title   string `json:"title"`
+	Details string `json:"details"`
+}
+type ContentRepository interface {
+	//TODO 1
+	FindAll() ([]Content, error)
+	ById(string) (*Content,error)
+}
