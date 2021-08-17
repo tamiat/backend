e0f68f5e1f17d0e0611576eb6e7e8945ec17a7f0
RahmaYasser <rahma.y9079@gmail.com>
2021-08-17 22:23:41 +0200

    adding content repo for database

---
+++ domain/content/contentRepositoryDb.go
@@ -0,0 +1,75 @@
+package content
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"os"
+)
+
+type ContentRepositoryDb struct {
+	db *sql.DB
+}
+
+//TODO 2
+func (r ContentRepositoryDb) FindAll() ([]Content, error) {
+	findAllSQL := "SELECT * FROM contents"
+	rows, err := r.db.Query(findAllSQL)
+	if err != nil {
+		log.Println("error while querying " + err.Error())
+		return nil, err
+	}
+	contents := make([]Content, 0)
+	for rows.Next() {
+		var c Content
+		err := rows.Scan(&c.Id, &c.Title, &c.Details)
+		if err != nil {
+			log.Println("error while querying " + err.Error())
+			return nil, err
+		}
+		contents = append(contents, c)
+	}
+	return contents, nil
+}
+func (d ContentRepositoryDb) ById(id string) (*Content, error) {
+	contentSQL := "SELECT * FROM contents WHERE id = $1"
+	row := d.db.QueryRow(contentSQL, id)
+	var c Content
+	err := row.Scan(&c.Id, &c.Title, &c.Details)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, errors.New("content not found")
+		} else {
+			log.Println("error while querying in repoDB" + err.Error())
+			return nil, errors.New("unexpected database error")
+		}
+	}
+	return &c, nil
+}
+
+func NewContentRepositoryDb() ContentRepositoryDb {
+
+	dataSourceName := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s",
+		os.Getenv("HOST"),
+		os.Getenv("DBPORT"),
+		os.Getenv("DBNAME"),
+		os.Getenv("USER"),
+		os.Getenv("PASS"))
+	db, err := sql.Open("pgx", dataSourceName)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("unable to conect to db"))
+		panic(err)
+	}
+	log.Println("connected to db ")
+
+	//test connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("cannot ping db")
+		panic(err)
+	}
+	log.Println("pinged db")
+	return ContentRepositoryDb{db}
+}
