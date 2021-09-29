194c87eed29b4017aa235a5aee791d07cde051eb
RahmaYasser <rahma.y9070@gmail.com>
2021-09-29 02:52:11 +0200

    sendEmail func finished

--- pkg/emailVerification/codeGenerator.go
+++ pkg/emailVerification/codeGenerator.go
@@ -1,1 +1,13 @@
 package emailVerification
+
+import (
+	"github.com/xlzd/gotp"
+)
+
+func CodeGenerator() string{
+	secret:=gotp.RandomSecret(16)
+	//fmt.Println(secret)
+	totp := gotp.NewDefaultTOTP(secret)
+	code:= totp.Now()
+	return code
+}
--- pkg/emailVerification/sendGridApi.go
+++ pkg/emailVerification/sendGridApi.go
@@ -1,1 +1,44 @@
 package emailVerification
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func sendEmail(to string, code string) error{
+	//TODO remove email to be env variable
+	//TODO rename variables
+	//TODO add comments
+	from := "ryasser@egirna.net"
+	to = "rahma.y9079@gmail.com"
+	jsonText:=fmt.Sprintf("{\n  \"from\":{\"email\":\"%s\"},\n  \"personalizations\":[\n    {\n      \"to\":[\n        {\n          \"email\":\"%s\"\n        }\n      ],\n      \"dynamic_template_data\":{\n        \"code\": \"%s\"\n      }\n    }\n  ],\n  \"template_id\":\"d-de1aefebe42f43939bac714a95b8779e\"\n}\n",from,to,code)
+	var jsonReq2 = []byte(jsonText)
+	u:="https://api.sendgrid.com/v3/mail/send"
+	req, err := http.NewRequest("POST", u, bytes.NewBuffer(jsonReq2))
+	req.Header.Set("Content-Type", "application/json")
+	//TODO add SENDGRID_API_KEY env var
+	bearer:="Bearer " + os.Getenv("SENDGRID_API_KEY")
+	req.Header.Add("Authorization", bearer)
+
+	if err != nil {
+		fmt.Println("error while making request to sendgrid api")
+		return err
+	}
+	c := http.DefaultClient
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println("error while making request to sendgrid api")
+		return err
+	}
+	defer resp.Body.Close()
+	//_ code be replaced with response variable
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error in response of sendgrid api")
+		return err
+	}
+	return nil
+}
