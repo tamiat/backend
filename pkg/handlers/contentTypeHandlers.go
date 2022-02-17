707fb91957e39cfd916f9f3ebc62c286051ba5d4
RahmaYasser <rahma.y9079@gmail.com>
2022-02-17 18:16:15 +0200

    refactored http mux of createContentType into gin

--- pkg/handlers/contentTypeHandlers.go
+++ pkg/handlers/contentTypeHandlers.go
@@ -1,17 +1,12 @@
 package handlers
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
 
-	"github.com/gorilla/mux"
-
 	"github.com/tamiat/backend/pkg/errs"
-	"github.com/tamiat/backend/pkg/response"
 	"github.com/tamiat/backend/pkg/service"
 )
 
@@ -21,17 +16,24 @@ type ContentTypeHandlers struct {
 
 func (ch *ContentTypeHandlers) createContentType(ctx *gin.Context) {
 	var newContentType map[string]interface{}
-	ctx.ShouldBind(&newContentType)
+	err := ctx.ShouldBind(&newContentType)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	//w.Header().Set("Content-Type", "application/json")
 	//params := mux.Vars(r)
 	userId, err := strconv.Atoi(ctx.Param("id"))
-
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": errs.ErrParsingID.Error()})
+		return
+	}
 	//userId := params["userId"]
 	//var newContentType interface{} // The interface where we will save the converted JSON data.
 	/*buffer, err := ioutil.ReadAll(r.Body)
 	err = r.Body.Close()
 	err = json.Unmarshal(buffer, &newContentType)
-	m := newContentType.(map[string]interface{})*/ // To use the converted data we will need to convert it
+	m := newContentType.(map[string]interface{})*/// To use the converted data we will need to convert it
 	// into a map[string]interface{}
 	var name, col string
 	name = ""
@@ -46,9 +48,7 @@ func (ch *ContentTypeHandlers) createContentType(ctx *gin.Context) {
 		}
 	}
 	if name == "" {
-		ctx.JSON(http.StatusBadRequest, errs.ErrNoContentTypeName)
-		//w.WriteHeader(http.StatusBadRequest)
-		//json.NewEncoder(w).Encode(response.NewResponse(errs.ErrNoContentTypeName.Error(),http.StatusBadRequest))
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": errs.ErrNoContentTypeName.Error()})
 		return
 	}
 	col = col[0 : len(col)-1]
@@ -56,12 +56,10 @@ func (ch *ContentTypeHandlers) createContentType(ctx *gin.Context) {
 	id, err = ch.service.CreateContentType(userId, name, col)
 	if err != nil {
 		if err == errs.ErrUnauthorized {
-			w.WriteHeader(http.StatusUnauthorized)
-			json.NewEncoder(w).Encode(response.NewResponse(err.Error(), http.StatusUnauthorized))
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": errs.ErrUnauthorized.Error()})
 			return
 		}
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(response.NewResponse(err.Error(), http.StatusInternalServerError))
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": errs.ErrServerErr.Error()})
 		return
 	}
 	type ID struct {
@@ -69,12 +67,13 @@ func (ch *ContentTypeHandlers) createContentType(ctx *gin.Context) {
 	}
 	var IDobj ID
 	IDobj.ID = id
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(IDobj)
+	//w.Header().Set("Content-Type", "application/json")
+	//json.NewEncoder(w).Encode(IDobj)
+	ctx.JSON(http.StatusOK, IDobj)
 	return
 }
 
-func (ch *ContentTypeHandlers) deleteContentType(w http.ResponseWriter, r *http.Request) {
+/*func (ch *ContentTypeHandlers) deleteContentType(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	userId := params["userId"]
@@ -227,3 +226,4 @@ func (ch *ContentTypeHandlers) deleteCol(w http.ResponseWriter, r *http.Request)
 	json.NewEncoder(w).Encode(response.NewResponse("This column has been deleted successfully", http.StatusOK))
 	return
 }
+*/
